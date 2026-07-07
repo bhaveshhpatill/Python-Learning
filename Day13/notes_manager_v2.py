cecf3cd6216bed811df4fdf35990e39936f0d8f7
Bhavesh <bhaveshpatil1685@gmail.com>
2026-07-07 20:51:01 +0530

    Day 13: Built Notes Manager V2 with file handling

---
+++ Day13/notes_manager_v2.py
@@ -0,0 +1,82 @@
+# ===== Notes Manager =====
+
+# 1. View Files
+# 2. Open File
+# 3. Create File
+# 4. Exit
+
+
+
+print("===== Notes Manager =====")
+import os
+
+
+def menu():
+    print("\n1. View Files")
+    print("2. Open File")
+    print("3. Create File")
+    print("4. Exit")
+
+
+def view_files():
+    notes = os.listdir(".")
+    print("\nFiles:")
+    txt_files = []
+
+    for note in notes:
+        if note.endswith(".txt"):
+            txt_files.append(note)
+
+    for i, note in enumerate(txt_files, start=1):
+        print(f"{i}. {note}")
+
+    return txt_files
+
+
+def open_file():
+    notes = view_files()
+    choice = int(input("\nEnter the file number to open: "))
+
+    if 1 <= choice <= len(notes):
+        filename = notes[choice - 1]
+
+        with open(filename, "r") as file:
+            content = file.read()
+
+        print(f"\nContent of {filename}:\n{content}")
+    else:
+        print("Invalid choice.")
+
+
+def create_file():
+    filename = input("\nEnter the name of the new file: ")
+
+    if not filename.endswith(".txt"):
+        filename += ".txt"
+
+    with open(filename, "w") as file:
+        content = input("Enter content for the file:\n")
+        file.write(content)
+
+    print(f"File '{filename}' created successfully.")
+
+
+def exit_program():
+    print("Exiting Notes Manager.")
+    exit()
+
+
+while True:
+    menu()
+    choice = input("\nEnter your choice: ")
+
+    if choice == "1":
+        view_files()
+    elif choice == "2":
+        open_file()
+    elif choice == "3":
+        create_file()
+    elif choice == "4":
+        exit_program()
+    else:
+        print("Invalid choice. Please try again.")
